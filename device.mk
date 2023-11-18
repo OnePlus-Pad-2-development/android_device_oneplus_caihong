25d5108e07529970ba33f87db91778c1e1175e41
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-17 21:19:02 -0500

    caihong: Opt out of GMS comms suite
    Change-Id: I7ebd0960ac9adad758ecb5ca1a3660f73447a9d1

--- device.mk
+++ device.mk
@@ -44,6 +44,9 @@ $(call soong_config_set_bool,recovery,target_recovery_uses_qti_drm,true)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23976/build.default.prop:$(TARGET_COPY_OUT_ODM)/etc/23976/build.default.prop
 
+# GMS
+WITH_GMS_COMMS_SUITE := false
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
