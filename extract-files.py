de8d0574d9af59a04f0eab3eb59ad1f98ac09a49
inferno0230 <mail@inferno0230.in>
2025-04-29 11:33:41 +0530

    caihong: Initial device tree conversion
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- extract-files.py
+++ extract-files.py
@@ -98,7 +98,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'waffle',
+    'caihong',
     'oneplus',
     namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
---
+++ lineage_caihong.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2021-2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from caihong device
+$(call inherit-product, device/oneplus/caihong/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Define target as tablet
+TARGET_IS_TABLET := true
+
+PRODUCT_NAME := lineage_caihong
+PRODUCT_DEVICE := caihong
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := OPD2403
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="OPD2403EEA-user 15 UKQ1.231108.001 U.R4T3.1d6a534_1-6 release-keys" \
+    BuildFingerprint=OnePlus/OPD2403EEA/OP5DAAL1:15/UKQ1.231108.001/U.R4T3.1d6a534_1-6:user/release-keys \
+    DeviceName=OP5DAAL1 \
+    DeviceProduct=OPD2403 \
+    SystemDevice=OP5DAAL1 \
+    SystemName=OPD2403
