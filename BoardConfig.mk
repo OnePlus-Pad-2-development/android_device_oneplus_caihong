3d4e9606e7933a4c745e3bcb24bb0bf987d1dbed
LuK1337 <priv.luk@gmail.com>
2025-06-24 19:45:21 +0200

    caihong: Set recovery touchscreen rotation
    Change-Id: Ibab9204f4411424e737847dae8b72dbf8ae15011

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
+TARGET_RECOVERY_DEFAULT_TOUCH_ROTATION := ROTATION_RIGHT
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/caihong/BoardConfigVendor.mk
