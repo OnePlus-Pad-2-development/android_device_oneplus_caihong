710703cd57362701f07bd9919dbe26a930ae076e
inferno0230 <mail@inferno0230.in>
2025-09-15 20:51:35 +0530

    waffle: Add decommonised display stack
    Change-Id: If220dbe3bf26533131f9437c61823c9f18a89a71
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- extract-files.py
+++ extract-files.py
@@ -24,13 +24,25 @@ namespace_imports = [
     'vendor/oneplus/sm8650-common',
 ]
 
+def lib_fixup_all_suffixes(lib: str, partition: str, *args, **kwargs):
+    if partition == 'odm':
+        return f'{lib}_odm'
+    if partition == 'vendor':
+        return f'{lib}_vendor'
+    return lib
+
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
+        'vendor.oplus.hardware.displaypanelfeature-V1-ndk',
+    ): lib_fixup_all_suffixes,
+    (
         'com.qti.sensor.lyt808',
+        'libpwirisfeature',
+        'libpwirishalwrapper',
         'libarcsoft_triple_sat',
         'libarcsoft_triple_zoomtranslator',
         'libdualcam_optical_zoom_control',
@@ -39,7 +51,15 @@ lib_fixups: lib_fixups_user_type = {
         'libtriplecam_optical_zoom_control',
         'libtriplecam_video_optical_zoom',
         'vendor.oplus.hardware.cammidasservice-V1-ndk',
+        'vendor.oplus.hardware.displaycolorfeature-V1-ndk',
         'vendor.oplus.hardware.camera_rfi-V1-ndk',
+        'vendor.pixelworks.hardware.display@1.0',
+        'vendor.pixelworks.hardware.display@1.1',
+        'vendor.pixelworks.hardware.display@1.2',
+        'vendor.pixelworks.hardware.display-V2-ndk',
+        'vendor.pixelworks.hardware.feature-V1-ndk',
+        'vendor.pixelworks.hardware.feature@1.0',
+        'vendor.pixelworks.hardware.feature@1.1',
     ): lib_fixup_vendor_suffix,
 }
 
@@ -63,6 +83,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('remote_handle_open')
         .clear_symbol_version('remote_register_buf_attr')
         .clear_symbol_version('remote_register_buf'),
+    'vendor/lib64/libcwb_qcom_aidl.so': blob_fixup()
+        .add_needed('libui_shim.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
