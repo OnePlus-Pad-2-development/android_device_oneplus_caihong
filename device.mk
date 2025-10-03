cf645200863d3b030b5f1f81ab1ea7dac18b9d2f
Bruno Martins <bgcngm@gmail.com>
2025-10-03 17:27:34 +0100

    caihong: configs: Remap Pogo keyboard function keys
    "Microphone toggle", "Find/Search" and "Screen lock" function keys were
    unmapped in AOSP builds. This change fixes that issue.
    
    Change-Id: I89c0c3b0464f177f4151e9a7fa63e43b8a0db867
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ WITH_GMS_COMMS_SUITE := false
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/Vendor_22d9_Product_3869.idc:$(TARGET_COPY_OUT_ODM)/usr/idc/Vendor_22d9_Product_3869.idc
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/Vendor_22d9_Product_3869.kl:$(TARGET_COPY_OUT_ODM)/usr/keylayout/Vendor_22d9_Product_3869.kl
+
 # Livedisplay
 $(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
