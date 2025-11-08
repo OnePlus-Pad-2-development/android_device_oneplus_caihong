f7c0afd50110d1f7de93196a21bd370787ef4ff5
Bruno Martins <bgcngm@gmail.com>
2025-11-08 14:51:19 +0000

    caihong: Drop IDisplayPanelFeature prebuilts
    Reversed interface added to hardware/oplus.
    
    Change-Id: Ib8f3b399d527d62f2b21bee6f80afc3edbc0385c

--- extract-files.py
+++ extract-files.py
@@ -25,17 +25,12 @@ namespace_imports = [
     'vendor/qcom/opensource/commonsys-intf/display',
 ]
 
-def lib_fixup_odm_suffix(lib: str, partition: str, *args, **kwargs):
-    return f'{lib}_{partition}' if partition == 'odm' else None
 
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    (
-        'vendor.oplus.hardware.displaypanelfeature-V1-ndk',
-    ):lib_fixup_odm_suffix,
     (
         'libhwconfigurationutil',
         'libpwirisfeature',
